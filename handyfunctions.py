310bb0f764279f69cdc7f3c62c3a70102a5d98fc
Chronoflo <florian.c.delprat@gmail.com>
2018-04-26 18:06:33 +0600

    HA. C'était parce que j'avais bidouillé l'heure du pc

--- handyfunctions.py
+++ handyfunctions.py
@@ -15,7 +15,7 @@ MOUSE = 2
 
 line_width = len("//SuperServeur::2018/04/06::00:03:48 : Application terminée.") * 3
 
-user_names = ['Robert', 'Sartres', 'Oui-Oui', 'Jean-Phillipe', 'Gros Jean', 'Martine']
+user_names = ['Robert', 'Sartres', 'Oui-Oui', 'Jean-Phillipe', 'Gros Jean', 'Martine', 'La Grosse Bertha']
 # log = None
 # eventLog = None
 
