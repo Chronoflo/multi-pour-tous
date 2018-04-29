f01c8b5a39f72f908bee65a1d78bc4d7e4b53b57
Chronoflo <florian.c.delprat@gmail.com>
2018-04-29 19:41:12 +0200

    Changements

--- modules/ztest_sdl.py
+++ modules/ztest_sdl.py
@@ -1,8 +1,10 @@
 import sys
+from math import cos
 
 from modules.handyfunctions import get_modules_path, ensure_path, os_adapt
-
+from modules.easydependencies import setup_third_party
 try:
+    setup_third_party()
     import sdl2.ext
 except ImportError:
     from modules.easydependencies import install_requirements
@@ -10,30 +12,149 @@ except ImportError:
 
     import sdl2.ext
 
-ressources_path = get_modules_path() + '/../pictures'
-ensure_path(ressources_path)
-RESOURCES = sdl2.ext.Resources(__file__, os_adapt(ressources_path))
+pic_folder = get_modules_path() + '/../pictures/'
 
-sdl2.ext.init()
 
-window = sdl2.ext.Window("Hello World!", size=(640, 480))
-window.show()
+def create_sprite(pic_path, window):
+    factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
+    sprite = factory.from_image(pic_path)
+    spriterenderer = factory.create_sprite_render_system(window)
+    spriterenderer.render(sprite)
+    return sprite
 
-factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
-sprite = factory.from_image(RESOURCES.get_path("smiley.bmp"))
 
-spriterenderer = factory.create_sprite_render_system(window)
-spriterenderer.render(sprite)
+def move_sprite(sprite, x, y):
+    sprite.position = sprite.x + x, sprite.y + y
 
-# will cause the renderer to draw the sprite 10px to the right and
-# 20 px to the bottom
-sprite.position = 10, 20
 
-# will cause the renderer to draw the sprite 55px to the right and
-# 10 px to the bottom
-sprite.position = 55, 10
+WHITE = sdl2.ext.Color(255, 255, 255)
 
-processor = sdl2.ext.TestEventProcessor()
-processor.run(window)
 
-sdl2.ext.quit()
+class SoftwareRenderer(sdl2.ext.SoftwareSpriteRenderSystem):
+    def __init__(self, window):
+        super(SoftwareRenderer, self).__init__(window)
+
+    def render(self, components):
+        sdl2.ext.fill(self.surface, sdl2.ext.Color(0, 0, 0))
+        super(SoftwareRenderer, self).render(components)
+
+
+class MovementSystem(sdl2.ext.Applicator):
+    def __init__(self, minx, miny, maxx, maxy):
+        super(MovementSystem, self).__init__()
+        self.componenttypes = Velocity, sdl2.ext.Sprite
+        self.minx = minx
+        self.miny = miny
+        self.maxx = maxx
+        self.maxy = maxy
+
+    def process(self, world, componentsets):
+        for velocity, sprite in componentsets:
+            swidth, sheight = sprite.size
+            sprite.x += velocity.vx
+            sprite.y += velocity.vy
+
+            sprite.x = max(self.minx, sprite.x)
+            sprite.y = max(self.miny, sprite.y)
+
+            pmaxx = sprite.x + swidth
+            pmaxy = sprite.y + sheight
+            if pmaxx > self.maxx:
+                sprite.x = self.maxx - swidth
+            if pmaxy > self.maxy:
+                sprite.y = self.maxy - sheight
+
+
+class CollisionSystem(sdl2.ext.Applicator):
+    def __init__(self, minx, miny, maxx, maxy):
+        super(CollisionSystem, self).__init__()
+        self.componenttypes = Velocity, sdl2.ext.Sprite
+        self.ball = None
+        self.minx = minx
+        self.miny = miny
+        self.maxx = maxx
+        self.maxy = maxy
+
+    def _overlap(self, item):
+        pos, sprite = item
+        if sprite == self.ball.sprite:
+            return False
+
+        left, top, right, bottom = sprite.area
+        bleft, btop, bright, bbottom = self.ball.sprite.area
+
+        return (bleft < right and bright > left and
+                btop < bottom and bbottom > top)
+
+    def process(self, world, componentsets):
+        collitems = [comp for comp in componentsets if self._overlap(comp)]
+        if collitems:
+            self.ball.velocity.vx = -self.ball.velocity.vx
+
+
+class Velocity(object):
+    def __init__(self):
+        super(Velocity, self).__init__()
+        self.vx = 0
+        self.vy = 0
+
+
+class Player(sdl2.ext.Entity):
+    def __init__(self, world, sprite, posx=0, posy=0):
+        self.sprite = sprite
+        self.sprite.position = posx, posy
+        self.velocity = Velocity()
+
+
+class Ball(sdl2.ext.Entity):
+    def __init__(self, world, sprite, posx=0, posy=0):
+        self.sprite = sprite
+        self.sprite.position = posx, posy
+        self.velocity = Velocity()
+
+
+def run():
+    sdl2.ext.init()
+    window = sdl2.ext.Window("The Pong Game", size=(800, 600))
+    window.show()
+
+    world = sdl2.ext.World()
+
+    movement = MovementSystem(0, 0, 800, 600)
+    collision = CollisionSystem(0, 0, 800, 600)
+    spriterenderer = SoftwareRenderer(window)
+    world.add_system(movement)
+    world.add_system(collision)
+    world.add_system(spriterenderer)
+
+    factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
+    sp_paddle1 = factory.from_color(WHITE, size=(20, 100))
+    sp_paddle2 = factory.from_color(WHITE, size=(20, 100))
+    sp_ball = factory.from_color(WHITE, size=(20, 20))
+
+    player1 = Player(world, sp_paddle1, 0, 250)
+    player2 = Player(world, sp_paddle2, 780, 250)
+    ball = Ball(world, sp_ball, 390, 290)
+    ball.velocity.vx = -10
+
+    collision.ball = ball
+
+    # create_sprite(pic_folder + 'smiley.bmp', window)
+    i = 0
+    running = True
+    while running:
+        i += 0.4
+        ball.sprite.y += int(5 * cos(i))
+        events = sdl2.ext.get_events()
+        for event in events:
+            if event.type == sdl2.SDL_QUIT:
+                running = False
+                break
+        sdl2.SDL_Delay(10)
+        world.process()
+        window.refresh()
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(run())
