53650f128b45c4b4c5452341f2568b1ac52ef565
Lalie <laliesurleweb@gmail.com>
2018-05-19 12:55:18 +0200

    j'ai commencé à faire le nv client

--- modules/test_nouveau_client.py
+++ modules/test_nouveau_client.py
@@ -3,9 +3,10 @@ import socket
 
 # Interface de creation d'utilisateur #
 class Fenetre_Connexion(MyFrame):
-    def __init__(self, fenetre, **kwargs):
+    def __init__(self, **kwargs):
         MyFrame.__init__(self, fenetre, **kwargs)
-        self.pack(fill='both')
+        self.pack(fill='both', expand="yes")
+
         # Creation du cadre principal #
         self.fenetre_connexion = MyLabelFrame(self, text=" Connexion ", labelanchor="nw", padx=20, pady=20, **kwargs)
         self.fenetre_connexion.pack(fill='both', expand="yes")
@@ -16,11 +17,11 @@ class Fenetre_Connexion(MyFrame):
 
         # Creation de la zone d'ecriture de l'adresse du serveur #
         self.HOST = tk.StringVar()
-        self.HOST = tk.Entry(self.fenetre_connexion, textvariable=self.HOST, width=30)
+        self.HOST = tk.Entry(self.fenetre_connexion, textvariable=self.HOST, width=15)
         self.HOST.pack()
 
         # Creation du Warning "Adresse du serveur" #
-        self.warning_adresse_serveur = tk.Label(self.fenetre_connexion, fg="red", text="Veuillez taper une adresse valide, de la forme : XXX.XXX.XXX.XXX")
+        self.warning_adresse_serveur = tk.Label(self.fenetre_connexion, fg="red", text="Veuillez entrer une adresse valide, de la forme : XXX.XXX.XXX.XXX")
 
         # Creation de la zone de texte "Port du serveur" #
         self.texte_port_serveur = tk.Label(self.fenetre_connexion, text="Port du serveur : ")
@@ -28,33 +29,45 @@ class Fenetre_Connexion(MyFrame):
 
         # Creation de la zone d'ecriture du port du serveur #
         self.PORT = tk.StringVar()
-        self.PORT = tk.Entry(self.fenetre_connexion, textvariable=self.PORT, width=30)
+        self.PORT = tk.Entry(self.fenetre_connexion, textvariable=self.PORT, width=6)
         self.PORT.pack()
 
         # Creation du Warning "Port du serveur" #
-        self.warning_port_serveur = tk.Label(self.fenetre_connexion, fg="red", text="Veuillez taper un port valide")
+        self.warning_port_serveur = tk.Label(self.fenetre_connexion, fg="red", text="Veuillez entrer un port valide")
 
         # Creation du bouton de connexion #
-        self.bouton_connexion = MyButton(self.fenetre_connexion, text="Connexion", command=self.connexion())
+        self.bouton_connexion = MyButton(self.fenetre_connexion, text="Connexion", command= lambda: self.connexion(self.HOST.get(), self.PORT.get()))
         self.bouton_connexion.pack()
 
-    def connexion(self):
-        self.warning_adresse_serveur.destroy()
-        self.warning_port_serveur.destroy()
+    def connexion(self, HOST, PORT):
+        print(HOST + "\n")
+        print(PORT)
 
-        HOST = str(self.HOST.get())
-        PORT = int(self.PORT.get())
+        self.warning_adresse_serveur.pack_forget()
+        self.warning_port_serveur.pack_forget()
 
-        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            tcp.connect((HOST, PORT))
-        except TimeoutError:
+        if HOST != "" and PORT != "":
+            try:
+                HOST = str(HOST)
+            except ValueError:
+                self.warning_adresse_serveur.pack()
+            try:
+                PORT = int(PORT)
+            except ValueError:
+                self.warning_port_serveur.pack()
+        else:
             pass
+
+#        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#        try:
+#            tcp.connect((HOST, PORT))
+#        except TimeoutError:
+#            pass
 
 
 
 fenetre = MyTkApp()
 fenetre.title("Client v0.1a")
 
-interface = Fenetre_Connexion(fenetre)
+interface = Fenetre_Connexion()
 interface.mainloop()
